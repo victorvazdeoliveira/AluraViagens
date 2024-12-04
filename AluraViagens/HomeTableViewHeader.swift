b5431f0490c3bad42683c0549b48d3d724f64e97
Victor Vaz de Oliveira <contato@victorvazdeoliveira.com.br>
2024-12-04 17:28:20 -0300

    Aula 1 "Criando um novo projeto" e aula 2 "Cabeçalho do aplicativo #1" concluídas, pude aprender sobre: - Implementação da UITableView. - Cabeçalho da TableView.

---
+++ AluraViagens/HomeTableViewHeader.swift
@@ -0,0 +1,18 @@
+//
+//  HomeTableViewHeader.swift
+//  AluraViagens
+//
+//  Created by Victor Vaz de Oliveira on 04/12/24.
+//
+
+import UIKit
+
+class HomeTableViewHeader: UIView {
+
+    // MARK: - IBOutlets
+    @IBOutlet weak var tituloLabel: UILabel!
+    @IBOutlet weak var headerView: UIView!
+    @IBOutlet weak var bannerImageView: UIImageView!
+    
+    
+}
